af3911e32178319b3d8fb1fcb91d1514be8f8982
ericnyirimana <ericnyirimana31@gmail.com>
2021-07-03 13:44:08 +0200

    PROP-2-f(service): API Integration for getting properties - Add new ENV variables. - Create a service which fetch paginated properties. [Finishes #2]

---
+++ app/Services/PropertyService.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Http;
+
+class PropertyService{
+
+
+    public function __construct(){
+
+    }
+
+
+
+    public function getPropertiesPerPage(int $pageNumber, int $pageSize){
+
+
+        $fetchPropertiesUrl = env('PROPERTY_API').'/properties';
+        try{
+            $response = Http::get($fetchPropertiesUrl, [
+                'api_key' => env('PROPERTY_API_KEY'),
+                'page[number]' => $pageNumber,
+                'page[size]' => $pageSize,
+            ]);
+        } catch(\Throwable $e){
+            response()->json(['error' => 'Oops, something went wrong'], 400);
+            Log::warning('An error has occurred when getting properties per page : {' . $e->getMessage() . '})');
+        }
+        return $response;
+    }
+
+}
