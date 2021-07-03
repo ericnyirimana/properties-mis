d855d9188fd0fc0e78f72b220ec49e22aef65c6a
ericnyirimana <ericnyirimana31@gmail.com>
2021-07-03 13:04:55 +0200

    PROP-1-t(migration): Create Db models and migrations - Create properties and property type models. - Create properties and property type migrations. [Finishes #1]

---
+++ database/migrations/2021_07_03_105218_create_properties_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePropertiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('properties', function (Blueprint $table) {
+            $table->uuid('uuid');
+            $table->foreignId('property_type_id')->constrained('property_types');
+            $table->string('county');
+            $table->string('country');
+            $table->string('town');
+            $table->string('description');
+            $table->string('address');
+            $table->string('image_full');
+            $table->string('image_thumbnail');
+            $table->decimal('latitude', 10, 8);
+            $table->decimal('longitude', 11, 8);
+            $table->integer('num_bedrooms');
+            $table->integer('num_bathrooms');
+            $table->integer('price');
+            $table->enum('type', ['sale', 'rent']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('properties');
+    }
+}
